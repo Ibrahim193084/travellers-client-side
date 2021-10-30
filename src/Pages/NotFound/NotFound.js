9c10d86abaccb612feaf994bee06837e271d4c72
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-10-30 15:15:05 +0600

    fifth

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Footer from '../Shared/Footer/Footer';
+import Navigation from '../Shared/Navigation/Navigation';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className="bg-dark">
+            <Navigation/>
+            <img className="notfound" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQirwMo_RBSjS96pBQfEbpKLygfQWkWyfPLOpYpXT1m68SB2VhZ50b4i1QnahAQ5IDzdwM&usqp=CAU" alt="" />
+            <Footer></Footer>
         </div>
     );
 };
