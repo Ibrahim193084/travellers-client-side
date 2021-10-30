56a25a45044326c43227962bc1b20c3c44544a15
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-10-30 21:18:04 +0600

    seventh commit

--- src/Pages/Tours/Tours.js
+++ src/Pages/Tours/Tours.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Spinner } from 'react-bootstrap';
 import Tour from './Tour/Tour';
 
 const Tours = () => {
     const [tours, setTours]= useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(()=>{
+        setIsLoading(true)
         fetch('https://young-headland-50696.herokuapp.com/tours')
         .then(res => res.json())
         .then(data=>setTours(data))
+        .finally(()=>setIsLoading(false))
     },[])
     return (
         <div id="tours">
             <h1 className="text-center text-danger my-5">TOURS</h1>
-             <Row xs={1} md={3} className="g-2">
-                 {tours.map(tour => <Tour key={tour._id}tour={tour}></Tour>)}
-             </Row>
+            {
+                isLoading ?<Spinner style={{marginLeft: '850px'}} className="text-center" animation="border" variant="danger"></Spinner>
+                :
+                <Row xs={1} md={3} className="g-2">
+                {tours.map(tour => <Tour key={tour._id}tour={tour}></Tour>)}
+            </Row>
+            }
             
         </div>
     );
